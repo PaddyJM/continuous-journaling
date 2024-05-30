06feb21aad1d8d25d3e7c1977ac32fc98b1bfa4c
Patrick Morton <patrickjamesmorton@gmail.com>
2024-05-30 12:47:24 +0100

    Got basic text input card working

--- app/index.tsx
+++ app/index.tsx
@@ -1,90 +1,84 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import Animated, { FadeInUp, FadeOutDown, LayoutAnimationConfig } from 'react-native-reanimated';
-import { Info } from '~/lib/icons/Info';
-import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
-import { Button } from '~/components/ui/button';
+import * as React from "react";
+import { View } from "react-native";
+import Animated, {
+  FadeInUp,
+  FadeOutDown,
+  LayoutAnimationConfig,
+} from "react-native-reanimated";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { Button } from "~/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from '~/components/ui/card';
-import { Progress } from '~/components/ui/progress';
-import { Text } from '~/components/ui/text';
-import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
+} from "~/components/ui/card";
+import { Progress } from "~/components/ui/progress";
+import { Text } from "~/components/ui/text";
+import { Textarea } from "~/components/ui/textarea";
 
-const GITHUB_AVATAR_URI =
-  'https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg';
+const MAIN_ICON_URI =
+  "https://i.pinimg.com/originals/94/b9/52/94b952ee642584804eed931f8a643ba2.png";
 
 export default function Screen() {
-  const [progress, setProgress] = React.useState(78);
+  const [progress, setProgress] = React.useState(0);
 
-  function updateProgressValue() {
-    setProgress(Math.floor(Math.random() * 100));
-  }
+  const updateProgressValue = (e: any) => {
+    const value = e.target.value;
+    setProgress(value.length);
+  };
+
   return (
-    <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-      <Card className='w-full max-w-sm p-6 rounded-2xl'>
-        <CardHeader className='items-center'>
-          <Avatar alt="Rick Sanchez's Avatar" className='w-24 h-24'>
-            <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
+    <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
+      <Card className="w-full max-w-sm p-6 rounded-2xl">
+        <CardHeader className="items-center">
+          <Avatar alt="Rick Sanchez's Avatar" className="w-24 h-24">
+            <AvatarImage source={{ uri: MAIN_ICON_URI }} />
             <AvatarFallback>
               <Text>RS</Text>
             </AvatarFallback>
           </Avatar>
-          <View className='p-3' />
-          <CardTitle className='pb-2 text-center'>Rick Sanchez</CardTitle>
-          <View className='flex-row'>
-            <CardDescription className='text-base font-semibold'>Scientist</CardDescription>
-            <Tooltip delayDuration={150}>
-              <TooltipTrigger className='px-2 pb-0.5 active:opacity-50'>
-                <Info size={14} strokeWidth={2.5} className='w-4 h-4 text-foreground/70' />
-              </TooltipTrigger>
-              <TooltipContent className='py-2 px-4 shadow'>
-                <Text className='native:text-lg'>Freelance</Text>
-              </TooltipContent>
-            </Tooltip>
-          </View>
+          <View className="p-3" />
+
+          <CardTitle className="pb-2 text-center">
+            What went well today?
+          </CardTitle>
         </CardHeader>
         <CardContent>
-          <View className='flex-row justify-around gap-3'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Dimension</Text>
-              <Text className='text-xl font-semibold'>C-137</Text>
-            </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Age</Text>
-              <Text className='text-xl font-semibold'>70</Text>
-            </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Species</Text>
-              <Text className='text-xl font-semibold'>Human</Text>
-            </View>
+          <View className="flex-row justify-around gap-3">
+            <Textarea
+              className="text-sm text-muted-foreground"
+              onChange={updateProgressValue}
+            ></Textarea>
           </View>
         </CardContent>
-        <CardFooter className='flex-col gap-3 pb-0'>
-          <View className='flex-row items-center overflow-hidden'>
-            <Text className='text-sm text-muted-foreground'>Productivity:</Text>
+        <CardFooter className="flex-col gap-3 pb-0">
+          <View className="flex-row items-center overflow-hidden">
+            <Text className="text-sm text-muted-foreground">Completed:</Text>
             <LayoutAnimationConfig skipEntering>
               <Animated.View
                 key={progress}
                 entering={FadeInUp}
                 exiting={FadeOutDown}
-                className='w-11 items-center'
+                className="w-11 items-center"
               >
-                <Text className='text-sm font-bold text-sky-600'>{progress}%</Text>
+                <Text className="text-sm font-bold text-sky-600">
+                  {progress}%
+                </Text>
               </Animated.View>
             </LayoutAnimationConfig>
           </View>
-          <Progress value={progress} className='h-2' indicatorClassName='bg-sky-600' />
+          <Progress
+            value={progress}
+            className="h-2"
+            indicatorClassName="bg-sky-600"
+          />
           <View />
           <Button
-            variant='outline'
-            className='shadow shadow-foreground/5'
-            onPress={updateProgressValue}
+            variant="outline"
+            className="shadow shadow-foreground/5"
+            onPress={() => {}}
           >
             <Text>Update</Text>
           </Button>
